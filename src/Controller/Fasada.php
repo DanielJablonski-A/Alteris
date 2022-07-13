aaf17e5f9b05fdfebc7349c202a34adea9dd26cf
Daniel Jablonski <daniel@jablonski.ovh>
2022-07-13 07:45:54 +0200

    dodane DocBlock do Fasady

--- src/Controller/Fasada.php
+++ src/Controller/Fasada.php
@@ -21,11 +21,22 @@ class Fasada
         $this->unitsObj = new UnitClass();
     }
 
+
+    /**
+    * Dodaje naszą ukończoną strukturę drzewiastą do obiektu, by móc na niej pracować
+    * w obrębie obiektu.
+    */ 
     public function addTreeObj($treeObj):void
     {
         $this->materialGroupsObj = $treeObj;
     }
 
+
+    /**
+    * Dodaje nowy materiał
+    * wysyłam żadanie do fabryki by ta przygotowała mi objekt
+    * jaki chcę
+    */ 
     public function addMaterial(string $newMaterialName, string $newMaterialCode, string $materialGroup, string $materialUnit):bool
     {
         return (new MaterialFactory($this->materialObj, $this->materialGroupsObj, $this->unitsObj))
@@ -33,6 +44,12 @@ class Fasada
             ->addMaterial($newMaterialName, $newMaterialCode, $materialGroup, $materialUnit);
     }
 
+
+   /**
+   * Edytuję materiał
+   * wysyłam żadanie do fabryki by ta zedytowała mi objekt
+   * jaki chcę
+   */ 
     public function editMaterial(string $materialName, string $materialGroup, string $materialUnit):bool
     {
         return (new MaterialFactory($this->materialObj, $this->materialGroupsObj, $this->unitsObj))
@@ -40,21 +57,37 @@ class Fasada
             ->editMaterial($materialName, $materialGroup, $materialUnit);
     }
 
+
+    /**
+    * Usuwam materiał który wcześniej mi fabryka przygotowała
+    */
     public function removeMaterial(string $MaterialName):void
     {
         $this->materialObj->removeMaterial($MaterialName);
     }
 
+
+   /**
+   * Mogę też usunąć grupe materiałową z drzewa które dodałem tu do obiektu
+   */ 
     public function removeMaterialGroup(string $materialGroupName):void
     {
         $this->materialGroupsObj->removeMaterialGroup($materialGroupName);
     }
 
+
+   /**
+   * Dodaje nową miarę do obiektu Unit
+   */ 
     public function addUnit(string $newUnit, string $newUnitShotcut):void
     {
         $this->unitsObj->addUnit($newUnit, $newUnitShotcut);
     }
 
+
+    /**
+    * Edytuję miarę w obiekcie Unit
+    */ 
     public function editUnit(string $unitName, string $newUnitName, string $newUnitShotcut):void
     {
         $this->unitsObj->editUnit($unitName, $newUnitName, $newUnitShotcut);
